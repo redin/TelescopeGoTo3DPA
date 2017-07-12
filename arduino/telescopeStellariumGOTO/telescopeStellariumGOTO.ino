89e5a9f78fe6d38f3387ac2dbd053c7df2368018
rmredin <ricardo.redin@gmail.com>
2017-07-11 23:53:18 -0300

    created answerCurentRA

--- arduino/telescopeStellariumGOTO/telescopeStellariumGOTO.ino
+++ arduino/telescopeStellariumGOTO/telescopeStellariumGOTO.ino
@@ -1,19 +1,43 @@
 
 char incomingChar;
 String readCmd;
+long currentRA=0;
+long currentDEC=0;
+long h=0;
+long m=0;
+long s=0;
+
 
 void setup() {
-  // open the serial port:
   Serial.begin(9600);
-  //acquireUTC();
-  //ensureParked();
-  //Serial.end();
-  //connectStellarium();
+  currentRA=23L*3600L+59L*60L+58L;
+}
+void answerCurrentRA() {
+  h = currentRA/3600L;
+  m = (currentRA - (h*3600))/60L;
+  s = currentRA - (h*3600) -(m*60);
+  if(h <10){
+    Serial.write('0');
+  }
+  Serial.print(h);
+  Serial.print(':');
+  if(m <10){
+    Serial.print('0');
+  }
+  Serial.print(String(m,DEC));
+  Serial.print(':');
+ 
+  if(s <10){
+    Serial.print('0');
+  }
+  Serial.print(String(s,DEC));
+  Serial.print('#');
+  //Serial.print("10:00:00#");
 }
 
 void loop() {
   if(Serial.available()>0){
-    delay(1000);
+    delay(50);
     while(Serial.available() > 0){
       incomingChar = Serial.read();
       readCmd += incomingChar;
@@ -22,7 +46,7 @@ void loop() {
     //Serial.println("/Read");
   }else{
       if(readCmd.equals("#:GR#")){
-        Serial.print("10:00:00#");
+        answerCurrentRA();
         Serial.flush();
         readCmd="";
       }else if(readCmd.equals("#:GD#")){
@@ -33,17 +57,17 @@ void loop() {
         Serial.flush();
         readCmd="";
       } else if(readCmd.startsWith("#:Q#:Sr ")) {
-        Serial.print("1");
+        Serial.print("1#");
         Serial.flush();
         readCmd="";
       }else if(readCmd.startsWith(":Sd ")) {
         //Serial.print(readCmd);
-        Serial.print("1");
+        Serial.print("1#");
         Serial.flush();
         readCmd="";
       }else if(readCmd.startsWith(":MS#")) {
         //Serial.print(readCmd);
-        Serial.print("0");
+        Serial.print("0#");
         Serial.flush();
         readCmd="";
       }else if(readCmd.length() > 20){
