0486940a01dae053ea425e090c6d95e91c7ccf36
redin <ricardo.redin@gmail.com>
2017-07-30 01:09:35 -0300

    Adding stepper movement and comand updates

--- arduino/telescopeStellariumGOTO/telescopeStellariumGOTO.ino
+++ arduino/telescopeStellariumGOTO/telescopeStellariumGOTO.ino
@@ -1,9 +1,27 @@
 #include <math.h>
-#include <DS1307.h>
+#include <Stepper.h>
+#include <Wire.h>
+#include "RTClib.h"
 
-// Init the DS1307
-DS1307 rtc(A4, A5);
+const int days2MonthN[]= {0,31,59,90,120,151,181,212,243,273,304,334};
+const int days2MonthL[]= {0,31,60,91,121,152,182,213,244,274,305,335};
 
+const double days2YearN[]={6208.5,6573.5,6938.5,7303.5,7669.5};
+
+const int stepsPerRevolution = 32;
+const int stepsOne = 2048;
+const double stepsPerDegreeAZ = stepsOne/360.0;
+const double stepsPerDegreeALT = stepsOne/90.0;
+const double degreesPerStepAZ = 360.0/stepsOne;
+const double degreesPerStepALT = 90.0/stepsOne;
+int deltaAZsteps = 0;
+int deltaALTsteps = 0;
+int maxSpeed = 400;
+boolean parked = false;
+
+Stepper stepperAZ(stepsPerRevolution, 8,10,9,11);            
+Stepper stepperALT(stepsPerRevolution, 4,6,5,7);
+RTC_DS1307 rtc;
 
 char incomingChar;
 String readCmd;
@@ -19,27 +37,52 @@ float latitudeDEC=0;
 long longitude=0;
 float longitudeDEC=0;
 //LST = 100.46 + 0.985647 * d + long + 15*UT
-float lst;
-float targetALT=0;
-float targetAZ=0;
-float currentALT=0;
-float currentAZ=0;
-float ut=0;
+double lst;
+double targetALT=0;
+double targetAZ=0;
+double currentALT=0;
+double currentAZ=0;
+double ut=0;
 unsigned long currentMilis=0;
 unsigned long previousMilis=0;
-Time time;
+DateTime time;
+double decimalTime;
 
 
 void setup() {
-  Serial.begin(9600);
-  
   currentRA=23L*3600L+59L*60L+58L;
   currentDEC=-23L*3600L+59L*60L;
+  targetRA=23L*3600L+59L*60L+58L;
+  targetDEC=-23L*3600L+59L*60L;
   //-30.042140
   latitude = -30L*3600L+02L*60L+31L;
   //-51.210638
-  longitude = -51L*3600L+12L*60L+38L;
+  longitude = -51L*3600L+(-12L*60L)+(-38L);
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+  time = rtc.now();
+  decimalTime =(double)time.hour()+((double)time.minute()/60.0000)+((double)time.second()/3600.000000);
 }
+
+int toSteps(double value, boolean alt){
+  if(alt){
+    return value * stepsPerDegreeALT;
+  }else{
+    return value * stepsPerDegreeAZ;
+  }
+    
+}
+
 void answerCurrentRA() {
   h = currentRA/3600L;
   m = (currentRA - (h*3600L))/60L;
@@ -102,15 +145,26 @@ void parseTargetDEC(String targetD){
   }
 }
 
+double calcDaysSinceJ2000(int y, int m, int d, int hours, int minutes, int seconds){
+  double decFractionOfDay = decimalTime/24.000000;
+  //TODO: take into account leap years
+  int days2Month = days2MonthN[m-1];
+  double days2Year = days2YearN[y-2017];
+  return decFractionOfDay + days2Month + d + days2Year;
+}
+
 void calculateALT_AZ(){
-  float raDeg = targetRA;
-  float hourAngle = lst - raDeg;
+  double lh = targetRA/3600.0000;
+  double lm = (targetRA - (lh*3600.0000))/60.0000;
+  double ls = targetRA - (lh*3600.0000) -(lm*60.0000);
+  double raDeg = (double)lh + ((double)lm/60.0000)+ ((double)ls/3600.0000);;
+  double hourAngle = lst - raDeg;
   if(hourAngle < 0){
     hourAngle = hourAngle + 360.0F;
   }
-  float sinAlt = sin(targetDEC)*sin(latitudeDEC)+cos(targetDEC)*cos(latitudeDEC)*cos(hourAngle);
+  double sinAlt = sin(targetDEC)*sin(latitudeDEC)+cos(targetDEC)*cos(latitudeDEC)*cos(hourAngle);
   targetALT = asin(sinAlt);
-  float cosA = (sin(targetDEC) - sin(targetALT)*sin(latitudeDEC))/(cos(targetALT)*cos(latitudeDEC));
+  double cosA = (sin(targetDEC) - sin(targetALT)*sin(latitudeDEC))/(cos(targetALT)*cos(latitudeDEC));
   if(sin(hourAngle) < 0){
     targetAZ = acos(cosA);
   }else{
@@ -118,12 +172,56 @@ void calculateALT_AZ(){
   }
 }
 
+void moveMount(){
+  if(!parked){
+    deltaAZsteps = toSteps(targetAZ - currentAZ, false);
+    deltaALTsteps = toSteps(targetALT - currentALT, true);
+    if(deltaAZsteps > 0){
+      stepperAZ.setSpeed(min(deltaAZsteps, maxSpeed));
+      stepperAZ.step(1);
+      currentAZ+= degreesPerStepAZ;
+    }else if(deltaAZsteps < 0){
+      stepperAZ.setSpeed(min(abs(deltaAZsteps), maxSpeed));
+      stepperAZ.step(-1);
+      currentAZ-= degreesPerStepAZ;
+    }
+    if(deltaALTsteps > 0){
+      stepperALT.setSpeed(min(deltaALTsteps, maxSpeed));
+      stepperALT.step(1);
+      currentALT+= degreesPerStepALT;
+    }else if(deltaALTsteps < 0){
+      stepperALT.setSpeed(min(abs(deltaALTsteps), maxSpeed));
+      stepperALT.step(-1);
+      currentALT-= degreesPerStepALT;
+    }  
+  }
+}
+
+void calculateLST(){
+  double lh = longitude/3600.0000;
+  double lm = (longitude - (lh*3600.0000))/60.0000;
+  double ls = longitude - (lh*3600.0000) -(lm*60.0000);
+  double longDEC = lh + (lm/60.0000);
+//LST = 100.46 + 0.985647 * d + long + 15*UT
+  double daysJ2000 = calcDaysSinceJ2000(time.year(),time.month(),time.day(),time.hour(),time.minute(),time.second()); 
+  lst = (0.985647 * daysJ2000) + (15.0000 * decimalTime) + longDEC + 100.460000;
+  while(lst >360.0000){
+    lst-= 360.00000;
+  }
+}
+
 void loop() {
-  time = rtc.getTime();
-  //calculateLST();
-  //readCurrentALT_AZ();
+  currentMilis=millis();
+  if(currentMilis > (previousMilis + 1000)){
+    previousMilis = currentMilis;
+    time = rtc.now();
+    decimalTime =(double)time.hour()+((double)time.minute()/60.0000)+((double)time.second()/3600.000000);
+    calculateLST();
+  }
+  //Serial.print("ALT=");Serial.print(currentALT); Serial.print(" "); Serial.print(targetALT);  Serial.print(" "); Serial.print(deltaALTsteps); Serial.println(" "); 
+  //Serial.print("AZ=");Serial.print(currentAZ); Serial.print(" "); Serial.print(targetAZ);  Serial.print(" "); Serial.print(deltaAZsteps); Serial.println(" "); 
   calculateALT_AZ();
-  //
+  moveMount();
   if(Serial.available()>0){
     while(Serial.available() > 0){
       incomingChar = Serial.read();
@@ -139,35 +237,42 @@ void loop() {
         Serial.flush();
         readCmd="";
       }else if(readCmd.equals("#:GL#")){
-        if(time.hour < 10){
+        if(time.hour() < 10){
           Serial.print("0");
         }
-        Serial.print(time.hour);
+        Serial.print(time.hour(),DEC);
         Serial.print(":");
-        if(time.min < 10){
+        if(time.minute() < 10){
           Serial.print("0");
         }
-        Serial.print(time.min);
+        Serial.print(time.minute(),DEC);
         Serial.print(":");
-        if(time.sec < 10){
+        if(time.second() < 10){
           Serial.print("0");
         }
-        Serial.print(time.sec);
+        Serial.print(time.second(),DEC);
+        Serial.print("#");
+        Serial.flush();
+        readCmd="";
+      }else if(readCmd.equals("#:GS#")){
+        Serial.print(lst);
         Serial.print("#");
         Serial.flush();
         readCmd="";
       }else if(readCmd.startsWith("#:SL ") && readCmd.length() == 14){
+        //Set the local Time
         int ltH = readCmd.substring(5,7).toInt();
         int ltM = readCmd.substring(8,10).toInt();
         int ltS = readCmd.substring(11,13).toInt();
-        rtc.setTime(ltH,ltM,ltS);
+        rtc.adjust(DateTime(time.year(), time.month(), time.day(), ltH, ltM, ltS));
         Serial.flush();
         readCmd="";
       }else if(readCmd.startsWith("#:SC ") && readCmd.length() == 14){
+        //Change Handbox Date to MM/DD/YY
         int month = readCmd.substring(5,7).toInt();
         int day = readCmd.substring(8,10).toInt();
         int yr = readCmd.substring(11,13).toInt();
-        rtc.setDate(day,month,yr);
+        rtc.adjust(DateTime(yr, month, day, time.hour(), time.minute(), time.second()));
         Serial.flush();
         readCmd="";
       }else if(readCmd.startsWith("#:Q#:Sr ") && readCmd.length() == 17) {
@@ -187,7 +292,7 @@ void loop() {
         Serial.flush();
         readCmd="";
       }else if(readCmd.length() > 20){
-        //Serial.print(readCmd);
+        //Serial.println(readCmd);
         readCmd="";
       }
   }
