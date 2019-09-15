a7d11862954d1e0fd3a72c8074779ac38e8a755d
redin <ricardo.redin@gmail.com>
2019-09-15 20:39:57 -0300

    Lets use esp8266 and get all info from internet!

--- arduino/telescopeStellariumGOTO/telescopeStellariumGOTO.ino
+++ arduino/telescopeStellariumGOTO/telescopeStellariumGOTO.ino
@@ -1,12 +1,20 @@
 #include <math.h>
 #include <Stepper.h>
-#include <Wire.h>
-#include "RTClib.h"
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <Arduino_JSON.h>
 
-const int days2MonthN[]= {0,31,59,90,120,151,181,212,243,273,304,334};
-const int days2MonthL[]= {0,31,60,91,121,152,182,213,244,274,305,335};
+const char *ssid     = "network";
+const char *password = "passwd";
 
-const double days2YearN[]={6208.5,6573.5,6938.5,7303.5,7669.5};
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP);
+int timeOffset = -10800;
 
 const int stepsPerRevolution = 32;
 const int stepsOne = 2048;
@@ -19,9 +27,8 @@ int deltaALTsteps = 0;
 int maxSpeed = 400;
 boolean parked = false;
 
-Stepper stepperAZ(stepsPerRevolution, 8,10,9,11);            
-Stepper stepperALT(stepsPerRevolution, 4,6,5,7);
-RTC_DS1307 rtc;
+//Stepper stepperAZ(stepsPerRevolution, 8,10,9,11);            
+//Stepper stepperALT(stepsPerRevolution, 4,6,5,7);
 
 char incomingChar;
 String readCmd;
@@ -45,11 +52,20 @@ double currentAZ=0;
 double ut=0;
 unsigned long currentMilis=0;
 unsigned long previousMilis=0;
-DateTime time;
 double decimalTime;
 
-
 void setup() {
+
+  Serial.begin (115200);
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.setTimeOffset(timeOffset);
+  timeClient.begin();
   currentRA=23L*3600L+59L*60L+58L;
   currentDEC=-23L*3600L+59L*60L;
   targetRA=23L*3600L+59L*60L+58L;
@@ -58,20 +74,8 @@ void setup() {
   latitude = -30L*3600L+02L*60L+31L;
   //-51.210638
   longitude = -51L*3600L+(-12L*60L)+(-38L);
-  Serial.begin(9600);
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
 
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-  time = rtc.now();
-  decimalTime =(double)time.hour()+((double)time.minute()/60.0000)+((double)time.second()/3600.000000);
+  decimalTime =(double)timeClient.getHours()+((double)timeClient.getMinutes()/60.0000)+((double)timeClient.getSeconds()/3600.000000);
 }
 
 int toSteps(double value, boolean alt){
@@ -145,13 +149,14 @@ void parseTargetDEC(String targetD){
   }
 }
 
-double calcDaysSinceJ2000(int y, int m, int d, int hours, int minutes, int seconds){
-  double decFractionOfDay = decimalTime/24.000000;
-  //TODO: take into account leap years
-  int days2Month = days2MonthN[m-1];
-  double days2Year = days2YearN[y-2017];
-  return decFractionOfDay + days2Month + d + days2Year;
-}
+//double calcDaysSinceJ2000(int y, int m, int d, int hours, int minutes, int seconds){
+//  // lets use https://api.usno.navy.mil/jdconverter?date=today&time=now
+//  double decFractionOfDay = decimalTime/24.000000;
+//  //TODO: take into account leap years
+//  int days2Month = days2MonthN[m-1];
+//  double days2Year = days2YearN[y-2017];
+//  return decFractionOfDay + days2Month + d + days2Year;
+//}
 
 void calculateALT_AZ(){
   double lh = targetRA/3600.0000;
@@ -177,46 +182,89 @@ void moveMount(){
     deltaAZsteps = toSteps(targetAZ - currentAZ, false);
     deltaALTsteps = toSteps(targetALT - currentALT, true);
     if(deltaAZsteps > 0){
-      stepperAZ.setSpeed(min(deltaAZsteps, maxSpeed));
-      stepperAZ.step(1);
+      //stepperAZ.setSpeed(min(deltaAZsteps, maxSpeed));
+      //stepperAZ.step(1);
       currentAZ+= degreesPerStepAZ;
     }else if(deltaAZsteps < 0){
-      stepperAZ.setSpeed(min(abs(deltaAZsteps), maxSpeed));
-      stepperAZ.step(-1);
+      //stepperAZ.setSpeed(min(abs(deltaAZsteps), maxSpeed));
+      //stepperAZ.step(-1);
       currentAZ-= degreesPerStepAZ;
     }
     if(deltaALTsteps > 0){
-      stepperALT.setSpeed(min(deltaALTsteps, maxSpeed));
-      stepperALT.step(1);
+      //stepperALT.setSpeed(min(deltaALTsteps, maxSpeed));
+      //stepperALT.step(1);
       currentALT+= degreesPerStepALT;
     }else if(deltaALTsteps < 0){
-      stepperALT.setSpeed(min(abs(deltaALTsteps), maxSpeed));
-      stepperALT.step(-1);
+      //stepperALT.setSpeed(min(abs(deltaALTsteps), maxSpeed));
+      //stepperALT.step(-1);
       currentALT-= degreesPerStepALT;
     }  
   }
 }
 
 void calculateLST(){
+  //lets use https://api.usno.navy.mil/sidtime?date=today&coords=30.042140S,51.210638W&reps=1&intv_mag=1&intv_unit=seconds&time=now
+
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+  if (http.begin(*client, "https://api.usno.navy.mil/sidtime?date=today&coords=30.042140S,51.210638W&reps=1&intv_mag=1&intv_unit=seconds&time=now")) {
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = http.getString();
+        Serial.println(payload);
+        JSONVar myObject = JSON.parse(payload);
+        if (JSON.typeof(myObject) == "undefined") {
+          Serial.println("Parsing input failed!");
+        }
+        if (myObject.hasOwnProperty("properties")) {
+          JSONVar keys = myObject.keys();
+          JSONVar value = myObject["properties"];
+          JSONVar data = value["data"];
+          JSONVar data0 = data[0];
+          JSONVar lmst = data0["lmst"];
+          Serial.println(lmst);
+        }
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();
+  } else {
+    Serial.printf("[HTTP} Unable to connect\n");
+  }
+  
   double lh = longitude/3600.0000;
   double lm = (longitude - (lh*3600.0000))/60.0000;
   double ls = longitude - (lh*3600.0000) -(lm*60.0000);
   double longDEC = lh + (lm/60.0000);
 //LST = 100.46 + 0.985647 * d + long + 15*UT
-  double daysJ2000 = calcDaysSinceJ2000(time.year(),time.month(),time.day(),time.hour(),time.minute(),time.second()); 
-  lst = (0.985647 * daysJ2000) + (15.0000 * decimalTime) + longDEC + 100.460000;
+  //double daysJ2000 = calcDaysSinceJ2000(rtime.Year(),rtime.Month(),rtime.Day(),rtime.Hour(),rtime.Minute(),rtime.Second()); 
+  // lst = (0.985647 * daysJ2000) + (15.0000 * decimalTime) + longDEC + 100.460000;
   while(lst >360.0000){
     lst-= 360.00000;
   }
 }
 
 void loop() {
+
+  
   currentMilis=millis();
-  if(currentMilis > (previousMilis + 1000)){
+  if(currentMilis > (previousMilis + 5000)){
     previousMilis = currentMilis;
-    time = rtc.now();
-    decimalTime =(double)time.hour()+((double)time.minute()/60.0000)+((double)time.second()/3600.000000);
+    decimalTime =(double)timeClient.getHours()+(double)(timeClient.getMinutes()/60.0000)+(double)(timeClient.getSeconds()/3600.000000);
     calculateLST();
+    timeClient.update();
+    Serial.println(timeClient.getFormattedTime());
   }
   //Serial.print("ALT=");Serial.print(currentALT); Serial.print(" "); Serial.print(targetALT);  Serial.print(" "); Serial.print(deltaALTsteps); Serial.println(" "); 
   //Serial.print("AZ=");Serial.print(currentAZ); Serial.print(" "); Serial.print(targetAZ);  Serial.print(" "); Serial.print(deltaAZsteps); Serial.println(" "); 
@@ -237,20 +285,20 @@ void loop() {
         Serial.flush();
         readCmd="";
       }else if(readCmd.equals("#:GL#")){
-        if(time.hour() < 10){
+        if(timeClient.getHours() < 10){
           Serial.print("0");
         }
-        Serial.print(time.hour(),DEC);
+        Serial.print(timeClient.getHours(),DEC);
         Serial.print(":");
-        if(time.minute() < 10){
+        if(timeClient.getMinutes() < 10){
           Serial.print("0");
         }
-        Serial.print(time.minute(),DEC);
+        Serial.print(timeClient.getMinutes(),DEC);
         Serial.print(":");
-        if(time.second() < 10){
+        if(timeClient.getSeconds() < 10){
           Serial.print("0");
         }
-        Serial.print(time.second(),DEC);
+        Serial.print(timeClient.getSeconds(),DEC);
         Serial.print("#");
         Serial.flush();
         readCmd="";
@@ -264,7 +312,7 @@ void loop() {
         int ltH = readCmd.substring(5,7).toInt();
         int ltM = readCmd.substring(8,10).toInt();
         int ltS = readCmd.substring(11,13).toInt();
-        rtc.adjust(DateTime(time.year(), time.month(), time.day(), ltH, ltM, ltS));
+        //rtc.adjust(DateTime(time.year(), time.month(), time.day(), ltH, ltM, ltS));
         Serial.flush();
         readCmd="";
       }else if(readCmd.startsWith("#:SC ") && readCmd.length() == 14){
@@ -272,7 +320,7 @@ void loop() {
         int month = readCmd.substring(5,7).toInt();
         int day = readCmd.substring(8,10).toInt();
         int yr = readCmd.substring(11,13).toInt();
-        rtc.adjust(DateTime(yr, month, day, time.hour(), time.minute(), time.second()));
+        //rtc.adjust(DateTime(yr, month, day, rtime.Hour(), rtime.Minute(), rtime.Second()));
         Serial.flush();
         readCmd="";
       }else if(readCmd.startsWith("#:Q#:Sr ") && readCmd.length() == 17) {
